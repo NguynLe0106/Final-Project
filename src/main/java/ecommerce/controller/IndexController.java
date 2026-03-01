b79bb73e8b82b5e79210dfab7952db51932e39dd
Nero <thanhnguyen2409@pm.me>
2026-03-01 20:27:31 +0700

    init-commit

--- src/main/java/ecommerce/controller/IndexController.java
+++ src/main/java/ecommerce/controller/IndexController.java
@@ -18,61 +18,61 @@ public class IndexController
         return "index";
     }
 
-    @GetMapping("/signup")
-    public String signupPage(Model model)
-    {
-        model.addAttribute("currentPage", "signup");
-        return "signup";
-    }
-
-    @PostMapping("/signup")
-    public String signupSubmit()
-    {
-        return "redirect:/";
-    }
-
-    @GetMapping("/account")
-    public String accountPage(Model model)
-    {
-        model.addAttribute("currentPage", "account");
-        model.addAttribute("userDisplayName", "Md Rimel");
-        return "account";
-    }
-
-    @PostMapping("/account")
-    public String accountSubmit()
-    {
-        return "redirect:/account";
-    }
-
-    @GetMapping("/product")
-    public String productPage()
-    {
-        return "product";
-    }
-
-    @GetMapping("/contact")
-    public String contactPage(Model model)
-    {
-        model.addAttribute("currentPage", "contact");
-        return "contact";
-    }
-
-    @PostMapping("/contact")
-    public String contactSubmit()
-    {
-        return "redirect:/contact";
-    }
-
-    @GetMapping("/cart")
-    public String cartPage()
-    {
-        return "cart";
-    }
-
-    @PostMapping("/cart")
-    public String cartSubmit()
-    {
-        return "redirect:/cart";
-    }
+//    @GetMapping("/signup")
+//    public String signupPage(Model model)
+//    {
+//        model.addAttribute("currentPage", "signup");
+//        return "signup";
+//    }
+//
+//    @PostMapping("/signup")
+//    public String signupSubmit()
+//    {
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/account")
+//    public String accountPage(Model model)
+//    {
+//        model.addAttribute("currentPage", "account");
+//        model.addAttribute("userDisplayName", "Md Rimel");
+//        return "account";
+//    }
+//
+//    @PostMapping("/account")
+//    public String accountSubmit()
+//    {
+//        return "redirect:/account";
+//    }
+//
+//    @GetMapping("/product")
+//    public String productPage()
+//    {
+//        return "product";
+//    }
+//
+//    @GetMapping("/contact")
+//    public String contactPage(Model model)
+//    {
+//        model.addAttribute("currentPage", "contact");
+//        return "contact";
+//    }
+//
+//    @PostMapping("/contact")
+//    public String contactSubmit()
+//    {
+//        return "redirect:/contact";
+//    }
+//
+//    @GetMapping("/cart")
+//    public String cartPage()
+//    {
+//        return "cart";
+//    }
+//
+//    @PostMapping("/cart")
+//    public String cartSubmit()
+//    {
+//        return "redirect:/cart";
+//    }
 }
